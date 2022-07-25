e832aa8c7e025a9f9bed5778105369f8efb375df
Lukas Möller <lmoeller@student.ethz.ch>
2022-07-25 10:19:08 +0200

    Restore cross compilation toolchain

--- cmake/toolchains/linux-musl/toolchain.cmake
+++ cmake/toolchains/linux-musl/toolchain.cmake
@@ -12,13 +12,32 @@ set(COMPILER_BIN ${ROOT_DIR}llvm-project/build/bin/)
 
 set(CMAKE_C_COMPILER ${COMPILER_BIN}clang)
 set(CMAKE_CXX_COMPILER ${COMPILER_BIN}clang++)
+set(AR x86_64-unknown-linux-musl-ar)
+set(CMAKE_AR ${AR})
+set(RANLIB x86_64-unknown-linux-musl-ranlib)
+set(CMAKE_RANLIB ${RANLIB})
+
+set(DOCKER_IMAGE aws-example-tc)
+
+set(SYSROOT ${ROOT_DIR}cmake/toolchains/linux-musl/sysroot)
+set(OSX_SYSROOT ${SYSROOT})
+set(CMAKE_SYSROOT ${SYSROOT})
 
 set(CMAKE_CPP_COMPILER_TARGET "x86_64-alpine-linux-musl")
 
-set(COMMON_FLAGS "")
+set(COMMON_FLAGS "-funwind-tables --sysroot=${SYSROOT} --prefix=/usr/local/bin/x86_64-unknown-linux-musl- --target=x86_64-alpine-linux-musl")
 set(COMPILER_FLAGS "${COMMON_FLAGS} -O3")
 
-set(CMAKE_CXX_FLAGS "${COMPILER_FLAGS}")
+set(CMAKE_CXX_FLAGS "${COMPILER_FLAGS} -stdlib=libstdc++")
 # Convince libcurl that the target platform has socket and fcntl_o_nonblock
 set(CMAKE_C_FLAGS "${COMPILER_FLAGS} -DHAVE_SOCKET -DHAVE_FCNTL_O_NONBLOCK")
 
+set(LINK_FLAGS "${COMMON_FLAGS} -L${ROOT_DIR}cmake/toolchains/linux-musl/sysroot/lib --gcc-toolchain=${ROOT_DIR}cmake/toolchains/linux-musl/sysroot/usr")
+set(CMAKE_BUILD_TYPE "Debug")
+
+# these variables tell CMake to avoid using any binary it finds in 
+# the sysroot, while picking headers and libraries exclusively from it 
+set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_PACKAGE ONLY)
