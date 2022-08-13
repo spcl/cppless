74958994bd18ba7c343d79536b64afcd27e4e7d9
Lukas Möller <mail@lukas-moeller.ch>
2022-08-13 11:03:14 +0000

    Add GPLv2 to bots benchmark code

--- benchmarks/bots/knapsack/threads.cpp
+++ benchmarks/bots/knapsack/threads.cpp
@@ -1,3 +1,23 @@
+/**********************************************************************************************/
+/*  This program is part of the Barcelona OpenMP Tasks Suite                                  */
+/*  Copyright (C) 2009 Barcelona Supercomputing Center - Centro Nacional de Supercomputacion  */
+/*  Copyright (C) 2009 Universitat Politecnica de Catalunya                                   */
+/*                                                                                            */
+/*  This program is free software; you can redistribute it and/or modify                      */
+/*  it under the terms of the GNU General Public License as published by                      */
+/*  the Free Software Foundation; either version 2 of the License, or                         */
+/*  (at your option) any later version.                                                       */
+/*                                                                                            */
+/*  This program is distributed in the hope that it will be useful,                           */
+/*  but WITHOUT ANY WARRANTY; without even the implied warranty of                            */
+/*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                             */
+/*  GNU General Public License for more details.                                              */
+/*                                                                                            */
+/*  You should have received a copy of the GNU General Public License                         */
+/*  along with this program; if not, write to the Free Software                               */
+/*  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA            */
+/**********************************************************************************************/
+
 #include <algorithm>
 #include <limits>
 #include <memory>
@@ -26,7 +46,7 @@ inline auto knapsack_threads(unsigned int split,
     auto child_items = items.subspan(1);
     std::vector<knapsack_item> items_vector(child_items.begin(),
                                             child_items.end());
-    auto task = [](std::vector<knapsack_item> items, int c, int v) mutable
+    auto task = [](std::vector<knapsack_item> items, int c, int v)
     {
       int best_so_far = std::numeric_limits<int>::min();
       return knapsack_serial(best_so_far, items, c, v);
@@ -72,6 +92,7 @@ auto knapsack(threads_args args) -> int
 
   int res = std::numeric_limits<int>::min();
   for (auto& f : futures) {
+    std::cout << "partial: " << *f << std::endl;
     res = std::max(*f, res);
   }
   return res;
